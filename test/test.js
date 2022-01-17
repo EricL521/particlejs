c967a9f89e5063ca15575fd1c1af3edb3e31b77a
GCreeper00 <liueric390@gmail.com>
2022-01-16 23:12:02 -0500

    Added dragging to test

--- test/test.js
+++ test/test.js
@@ -1,16 +1,27 @@
 var svgns = "http://www.w3.org/2000/svg";
 const particleContainer = document.getElementById("particle-container");
-function componentToHex(c) {
-    let hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
+
+// allow dragging
+const mouseParticle = new Particle({
+    position: {x: 0, y: 0},
+    targetDistance: 0,
+    strength: 50,
+    mass: 100
+});
+window.addEventListener("mousemove", (e) => {
+    const size = particleContainer.getBoundingClientRect();
+    mouseParticle.position = {x: e.offsetX * 1920 / size.width, y: e.offsetY * 1080 / size.height};
+});
+let onUpdate = () => {
+    if (selectedParticle && 
+        selectedParticle.targetDistance + mouseParticle.targetDistance <= Math.sqrt(Math.pow(selectedParticle.position.x - mouseParticle.position.x, 2) + Math.pow(selectedParticle.position.y - mouseParticle.position.y, 2)))
+        selectedParticle.interact(mouseParticle, 1);
+};
 
 const system = new System(100, 20, 10, {
-    updateInterval: 7.5, // milliseconds
-    friction: Math.E/500
+    updateInterval: 5, // milliseconds
+    friction: Math.E/25,
+    onUpdate: onUpdate
 });
 
 let locations = [{
@@ -28,19 +39,20 @@ let velocities = [{
     x: 0, y: 0
 }];
 let masses = [];
-let distances = [30];
+let distances = []
 let strengths = [];
-for (let j = 0; j < 1; j ++)
-    for (let i = 0; i < 3; i ++) {
+const height = 4;
+for (let j = 0; j < 4; j ++)
+    for (let i = 0; i < height; i ++) {
         const particle = new Particle({
-            mass: masses[3*j+i]? masses[3*j+i]: 100,
+            mass: masses[height*j+i],
             position: {
-                x: 900 + 50*j,
-                y: 500 + 50*i
+                x: 800 + 55*j,
+                y: 400 + 55*i
             },
-            strength: strengths[3*j+i],
-            targetDistance: distances[3*j+i],
-            velocity: velocities[3*j+i]
+            strength: strengths[height*j+i],
+            targetDistance: distances[height*j+i],
+            velocity: velocities[height*j+i]
         }, (thisParticle) => {
             circle.setAttributeNS(null, "cx", thisParticle.position.x);
             circle.setAttributeNS(null, "cy", thisParticle.position.y);
@@ -49,13 +61,21 @@ for (let j = 0; j < 1; j ++)
         system.addParticle(particle);
 
         const circle = document.createElementNS(svgns, "circle");
-        circle.id = "particle" + (3*j+i);
+        circle.id = "particle" + (height*j+i);
         circle.setAttributeNS(null, "r", particle.radius);
         circle.setAttributeNS(null, "cx", particle.position.x);
         circle.setAttributeNS(null, "cy", particle.position.y);
         circle.setAttributeNS(null, "draggable", true);
-        circle.setAttributeNS(null, "fill", rgbToHex(particle.mass/2, particle.strength * 10, 0));
+        circle.setAttributeNS(null, "fill", `rgb(${particle.mass/2}, ${particle.strength * 10}, 0)`);
         particleContainer.appendChild(circle);
+
+        circle.addEventListener("mousedown", () => {
+            selectedParticle = particle;
+        });
     }
+let selectedParticle = null;
+window.addEventListener("mouseup", () => {
+    selectedParticle = null;
+});
 
 system.start();
